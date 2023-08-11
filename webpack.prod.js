cb5669b5733570d48a1c998f104a133b7f577420
Jack Brown <jack@jackbrown.io>
2023-08-10 17:14:39 -0700

    Updated styles/webpack.prod.

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   output: {
-    filename: 'plastos.b.js',
+    filename: 'solace_notes.b.js',
     path: path.resolve(__dirname, 'dist/'),
   }
 });
